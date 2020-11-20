d03153a7c2ef092f0e344e29a701517b59c6858b
Ubuntu <ubuntu@ip-172-31-30-241.ap-northeast-2.compute.internal>
2020-11-20 06:47:08 +0000

    server started

--- index.ts
+++ index.ts
@@ -1,2 +1,16 @@
 import * as Koa from "koa";
 import * as koaLogger from "koa-logger";
+import * as Router from "koa-router";
+
+const app = new Koa();
+const router = new Router();
+
+router.get("/", async (ctx, next) => {
+    ctx.body = {msg: "Hello "}
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(80,() => {
+	console.log("Server Started");
+});
